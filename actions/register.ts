c8ec2f841fbc3962d68f0ec0a9599324b1583fbb
Prashant Gupta <prashantguptaofficial007@gmail.com>
2025-03-22 15:22:42 +0530

    Added register user data stored in database

--- actions/register.ts
+++ actions/register.ts
@@ -1,15 +1,39 @@
 "use server"
 
+import bcrypt from "bcrypt"
 import * as z from "zod"
 import { RegisterSchema } from "@/schema"
+import { db } from "@/lib/db"
+import { getUserByEmail } from "@/data/user"
 
 
 export const register = async(values: z.infer<typeof RegisterSchema>) => {
+
     const validateFields = RegisterSchema.safeParse(values)
 
     if(!validateFields.success){
         return {error: "Inavlid fields!"}
     }
 
-    return {success: "Email sent!"}
+    const {email, password, name} = validateFields.data
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const existingUser = await getUserByEmail(email)
+
+    if(existingUser){
+        return {error: "Email already in use!"}
+    }
+
+    await db.user.create({
+        data:{
+            name,
+            email,
+            password: hashedPassword
+        }
+    })
+
+    // TODO: send verification token email
+
+    return {success: "User created!"}
 }
