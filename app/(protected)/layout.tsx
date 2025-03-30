a5b18d786e927b871a30e5a088bdb6238af265e1
Prashant Gupta <prashantguptaofficial007@gmail.com>
2025-03-30 17:55:33 +0530

    Add user button

---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,16 @@
+import Navbar from "./_components/navbar";
+
+interface ProtectedLayoutpage{
+    children: React.ReactNode
+}
+
+const ProtectedLayout = ({children}: ProtectedLayoutpage) => {
+  return (
+    <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-gradient-to-b from-sky-400 to-blue-800">
+      <Navbar />
+      {children}
+    </div>
+  )
+}
+
+export default ProtectedLayout;
--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,20 +1,20 @@
-import { auth, signOut } from "@/auth";
+"use client";
 
-const SettingPage = async ()=>{
-    const session  = await auth()
-    return (
-        <div>
-            {JSON.stringify(session)}
-            <form action={async()=>{
-                "use server"
-                await signOut()
-            }}>
-                <button type="submit">
-                    Sign Out
-                </button>
-            </form>
-        </div>
-    )
-}
+import { logout } from "@/actions/logout";
+import { useCurrentUser } from "@/hooks/use-current-user";
+
+const SettingPage = () => {
+  const user = useCurrentUser();
+
+  const onClick = () => {
+    logout()
+  };
+
+  return (
+    <div className="bg-white p-10 rounded-xl">
+      <button onClick={onClick} type="submit">Sign Out</button>
+    </div>
+  );
+};
 
 export default SettingPage;
